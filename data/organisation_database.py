a553e2bf33bcf1d5ccc9b8384a46101a28b0884e
Heyppy <maggioe@3il.fr>
2025-02-20 12:20:10 +0100

    Organisation Database
    Trie la database en mettant les images dans les dossiers des catégories (ID).
    
    Les dossiers Batchs ne sont pas supprimer au cas ou ils contiendraient des images qui n'ont pas était trouvé.
    
    Les images ayant plusieurs catégories sont dupliqué dans chaque dossier correspondant aux catégories de l'images. Pour une meilleur compréhension, une image dupliquer dans plusieurs catégories a le même nom dans chacun des dossiers catégories.

---
+++ data/organisation_database.py
@@ -0,0 +1,26 @@
+import json
+import os
+import shutil
+
+for id in range (60):
+    os.makedirs(str(id), exist_ok=True)
+
+with open("annotations.json", "r") as f:
+    coco_data = json.load(f)
+
+i = 0
+for image in coco_data["images"]:
+    id_img = image["id"]
+    file_name = image["file_name"]
+
+    if os.path.exists(file_name):
+        ext = os.path.splitext(file_name)[1]
+
+        for annotation in coco_data["annotations"]:
+            if annotation["image_id"] == id_img:
+                dest = os.path.join(str(annotation["category_id"]), f"{i}{ext}")
+                shutil.copy2(file_name, dest)
+
+        os.remove(file_name)
+    
+    i += 1
